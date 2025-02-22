10548469e955e2ee13a0082a115312d2c845a6e9
Nicole Price <nqp5198@psu.edu>
2025-02-22 13:32:23 -0500

    Initial commit

---
+++ src/main/java/model/Main.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * Nicole Price
+ *
+ * This project is designed for the user to be able to add multiple times together in hour:min:sec format
+ *
+ * The inspiration for this project was when I was trying to add times together and it took a while
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/model/Time.java
@@ -0,0 +1,41 @@
+package model;
+
+/**
+ * Each time the user enters will be an individual object
+ */
+
+public class Time {
+    public int hours;
+    public int minutes;
+    public int seconds;
+
+    public Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+}
